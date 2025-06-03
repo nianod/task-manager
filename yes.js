f95e654b97a0e77d82012cac09a2665beacfb0d2
nianod <arnoldkk422@gmail.com>
2025-06-03 17:52:42 +0300

    started logic

---
+++ yes.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('form')
+const taskInput = document.getElementById("teskentry")
+const taskList = document.querySelector(".task-list")
+
+
+form.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    const realTask = taskInput.value.trim()
+
+    if(realTask)
+
+})
