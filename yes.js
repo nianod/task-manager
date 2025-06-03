7c46f85ce2cbd4346e4218a1c8bd827d0f7b42c2
nianod <arnoldkk422@gmail.com>
2025-06-03 19:31:11 +0300

    started logic

--- yes.js
+++ yes.js
@@ -3,11 +3,37 @@ const taskInput = document.getElementById("teskentry")
 const taskList = document.querySelector(".task-list")
 
 
-form.addEventListener("click", (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault()
 
     const realTask = taskInput.value.trim()
 
-    if(realTask)
+    if(realTask) {
+        const newTask = document.createElement("li")
+        newTask.innerHTML = `
+            <input type="checkbox" />
+            <span class="task-text">${realTask} </span>
+            <div class="actions">
+            <button class="complete-btn" title="Mark as Complete">✓</button>
+            <button class="delete-btn" title="Delete Task">✖</button>
+            </div>
+        `
+        taskList.appendChild(newTask)
 
+
+        taskInput.value = "";
+    }
+
+   
 })
+
+taskList.addEventListener("click", (e) => {
+    const li = e.target.closest("li")
+
+    if(e.target.classList.contains('delete-btn')) {
+        li.remove()
+    } else if(e.target.classList.contains('complete-btn' || e.target.type === 'checkbox')) {
+        //li.classList.toggle('completed')
+    }
+})
+ 
